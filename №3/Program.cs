61379990f28b95621b5e1c04a193c738857b77d6
Kirkleve <102994769+Kirkleve@users.noreply.github.com>
2022-05-16 22:09:46 +0200

    all comolite

--- №3/Program.cs
+++ №3/Program.cs
@@ -8,7 +8,6 @@ int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите n: ");
 int n = Convert.ToInt32(Console.ReadLine());
 double[,] array = new double[m, n];
-double[] sum = new double[n];
 
 for (int i = 0; i < m; i++)
 {
@@ -27,12 +26,12 @@ for (int i = 0; i < m; i++)
     Console.WriteLine();
 }
 
-for (int i = 0; i < m; i++)
+for (int j = 0; j < n; j++)
 {
-    for (int j = 0; j < n; j++)
+    double sum = 0;
+    for (int i = 0; i < m; i++)
     {
-        Console.WriteLine(sum[i] += array[j,i]);     
+        sum += array[i,j];     
     }
-    Console.WriteLine(Math.Round(sum[i]/m,2));
-    Console.WriteLine();
+    Console.WriteLine($"Среденее арифметическое {j+1} столбца = {Math.Round(sum/m,2)}");
 }
